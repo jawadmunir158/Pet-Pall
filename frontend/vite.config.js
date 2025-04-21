20019959126951032b368fa64e18d804a8cf9a45
Jadi <127825377+jawadmunir158@users.noreply.github.com>
2025-04-21 11:22:56 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Pets/', // Correct the base path if your app is inside a subfolder
+  base: './', // ✅ this is what you want for Vercel
   server: {
     port: 5174,
-    open: true, // Automatically open browser on start
+    open: true,
   },
 });
