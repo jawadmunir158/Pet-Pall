f2acda587a9baa160583db5b3732c55772a241ad
Jadi <127825377+jawadmunir158@users.noreply.github.com>
2025-04-21 11:32:42 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
+// vite.config.js
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // ✅ this is what you want for Vercel
+  base: './', // Use relative path for production build
   server: {
     port: 5174,
     open: true,
